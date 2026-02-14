6b446ae64f0997ebd940234e42e3d779dc3d91c1
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2026-02-14 21:11:53 +0200

    feat(books): add Book entity and EF Core configuration.

---
+++ src/Domain/Entities/BooksModule/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Domain.Entities.BooksModule;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public DateTime PublicationDate { get; set; }
+}
---
+++ src/Infrastructure/Data/Configurations/BookConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Entities.BooksModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infrastructure.Data.Configurations;
+
+internal class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+       builder.Property(b => b.Title)
+              .IsRequired()
+              .HasMaxLength(200);
+    }
+}
